d65737da2f23a3a37ea009968a7926b4d0bfcead
shiori-nambu <shiori.nambu@esm.co.jp>
2023-01-02 15:54:30 +0900

    install react-native-paper and tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-black'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
     </View>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.tsx', './<custom directory such as src>/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
